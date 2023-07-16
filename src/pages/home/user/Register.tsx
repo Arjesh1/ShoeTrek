0c7f8b79b5d78e7474fd63d5dbfeb592d3094262
Arjesh <arjesh.khadka2@gmail.com>
2023-07-16 21:45:30 +1000

    register form data

--- src/pages/home/user/Register.tsx
+++ src/pages/home/user/Register.tsx
@@ -1,14 +1,57 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import MainLayout from '../../../components/layout/MainLayout'
 import "./user.css"
+import { log } from 'console';
+
+interface FormState {
+ 
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register = () => {
+
+  const [form, setForm] = useState<FormState>({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  
+
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) =>{
+    const { name, value } = e.target;
+    setForm((data) => ({
+      ...data,
+      [name]: value,
+    }));
+
+  }
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    console.log(form);
+    
+
+  }
+
+
   return (
     <>
 
 <MainLayout>
+  <div className="mt-12 mb-6">
+    <h1 className='text-center font-mono text-4xl'>Register</h1>
+  </div>
 
-    <div className='container shadow-2xl p-4 m-4 mx-auto grid grid-rows-2  gap-1  sm:mt-2 md:mt-2  lg:mt-2 xl:mt-2 2xl:mt-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 sm:grid-rows-1 md:grid-rows-1 lg:grid-rows-1 xl:grid-rows-1 2xl:grid-rows-1 '>
+    <div className='container  shadow-violet-200 shadow-2xl p-4 m-4 my-8 mx-auto grid grid-rows-2  gap-1  sm:mt-2 md:mt-2  lg:mt-2 xl:mt-2 2xl:mt-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 sm:grid-rows-1 md:grid-rows-1 lg:grid-rows-1 xl:grid-rows-1 2xl:grid-rows-1 '>
+
+    
 
         <div className='flex justify-end bg-red-700 register-img '>
          
@@ -17,70 +60,68 @@ const Register = () => {
         {/* register form */}
         <div className='p-3'>
         <div className=" ">
-          <h1 className=" text-4xl text-center ">Register</h1>
+          
          
-<form>
+<form onSubmit={handleOnSubmit}>
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label  className="block text-sm font-medium leading-6 text-gray-900">
                 First name
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="first-name"
+                  name="firstName"
+                  onChange={handleOnChange}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label  className="block text-sm font-medium leading-6 text-gray-900">
                 Last name
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
+                  name="lastName"
+                  onChange={handleOnChange}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label  className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
                   name="email"
                   type="email"
-                  autoComplete="email"
+                  onChange={handleOnChange}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label  className="block text-sm font-medium leading-6 text-gray-900">
                 Password
               </label>
               <div className="mt-2">
                 <input
-                  
                   name="password"
                   type="password"
-                  
+                  onChange={handleOnChange}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="col-span-full">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label  className="block text-sm font-medium leading-6 text-gray-900">
                 Confirm Password
               </label>
               <div className="mt-2">
@@ -88,6 +129,7 @@ const Register = () => {
                  
                   name="confirmPassword"
                   type="password"
+                  onChange={handleOnChange}
                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
