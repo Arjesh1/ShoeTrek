a51ff52e0e9901a9a9c0d27f3b96724bd040fa00
Arjesh <arjesh.khadka2@gmail.com>
2023-08-05 07:06:14 +1000

    password reset

--- src/components/modal/ForgetPassword.jsx
+++ src/components/modal/ForgetPassword.jsx
@@ -3,13 +3,37 @@ import { Dialog, Transition } from '@headlessui/react'
 import { BiSolidErrorCircle } from 'react-icons/bi'
 import { useDispatch, useSelector } from 'react-redux'
 import { setForgetPassword } from '../../system/cartSlice'
+import { sendPasswordResetEmail } from 'firebase/auth'
+import { auth } from '../../config/firebase-config'
+import { toast } from 'react-toastify'
 
 
 const ForgetPassword = () => {
     const {forgetPassword} = useSelector(state => state.system)
+    const [form, setForm] = useState({})
     const dispatch =  useDispatch()
 
   const cancelButtonRef = useRef(null)
+
+  const handleOnResetPasswordChange = (e) =>{
+    const {name, value} = e.target
+    setForm({...form, [name]: value})
+
+  }
+
+  const handleOnResetPasswordSubmit = (e) =>{
+    e.preventDefault()
+    try {
+      sendPasswordResetEmail(auth, form.email).then(resp =>{
+
+        toast.success("Password reset link has been send to your email. ")
+
+      })
+      
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <>
@@ -39,6 +63,7 @@ const ForgetPassword = () => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+              <form className="space-y-6 w-full  " method="POST" onSubmit={handleOnResetPasswordSubmit}>
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-yellow-100 sm:mx-0 sm:h-10 sm:w-10">
@@ -55,11 +80,11 @@ const ForgetPassword = () => {
                       </div>
                     </div>
                   </div>
-
-                  <div className=" flex justify-center ">
-                  <form className="space-y-6 w-full" action="#" method="POST">
-            <div>
-              <label htmlFor="email" className="block w-full text-sm font-medium leading-6 text-gray-900">
+                  
+                  <div className=" mt-4  ">
+                  
+            <div className='w-full col-span-full'>
+              <label htmlFor="email" className=" w-auto flex justify-center text-center  text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
               <div className="mt-2 w-full">
@@ -67,13 +92,13 @@ const ForgetPassword = () => {
                   id="email"
                   name="email"
                   type="email"
-                  autoComplete="email"
                   required
-                  className="block w-4/6 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleOnResetPasswordChange}
+                  className=" w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-            </form>
+            
 
                   </div>
 
@@ -81,9 +106,9 @@ const ForgetPassword = () => {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                   <button
-                    type="button"
+                    type="submit"
                     className="inline-flex w-full justify-center rounded-md bg-yellow-300 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-yellow-400 sm:ml-3 sm:w-auto"
-                    onClick={() => dispatch(setForgetPassword(false))}
+                    
                   >
                     Reset Password
                   </button>
@@ -96,6 +121,7 @@ const ForgetPassword = () => {
                     Cancel
                   </button>
                 </div>
+                </form>
               </Dialog.Panel>
             </Transition.Child>
           </div>
