670e13cffe6a946fbf926a6c4b75373ef452370c
Arjesh <arjesh.khadka2@gmail.com>
2023-08-07 06:56:58 +1000

    cart emptied after successful order.

--- src/pages/checkOut/checkoutAction.jsx
+++ src/pages/checkOut/checkoutAction.jsx
@@ -4,18 +4,14 @@ import { toast } from "react-toastify";
 import { setCartProd } from "../product/productSlice";
 
 
-//send message
+//add orders to db and assign ordernumber as pk.
 export const addOrderedProductAction = (form) => async(dispatch) => {
     try {
         
         await setDoc(doc(db, "orders", form.orderNumber ), form)
-           
             toast.success("Your order has been successful")
             dispatch(setCartProd([]))
-
     } catch (error) {
-        console.log(error);
         toast.error("Unable to make a order at this time. Try back again later")
-        
     }
   }
