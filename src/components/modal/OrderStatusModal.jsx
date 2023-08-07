c20c0f2e41398d1b291dda112f8c9507c59e8b7b
Arjesh <arjesh.khadka2@gmail.com>
2023-08-08 08:24:45 +1000

    payment failed modal based on status given

--- src/components/modal/OrderStatusModal.jsx
+++ src/components/modal/OrderStatusModal.jsx
@@ -7,6 +7,7 @@ import { sendPasswordResetEmail } from 'firebase/auth'
 import { auth } from '../../config/firebase-config'
 import { toast } from 'react-toastify'
 import { RootState } from '../../store'
+import { BiSolidErrorCircle } from 'react-icons/bi'
 
 
 const OrderStatusModal = () => {
@@ -15,6 +16,8 @@ const OrderStatusModal = () => {
   const cancelButtonRef = useRef(null)
   console.log(orderModal);
 
+  //status comes from the stripe
+  const status = "successful"
 
   
   
@@ -36,7 +39,7 @@ const OrderStatusModal = () => {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
+          <div className="flex min-h-full min-w-full items-end justify-center p-5 text-center items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -49,28 +52,42 @@ const OrderStatusModal = () => {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="flex flex-col items-center">
+
+                  {status === "successful" ? (
+                    <div className="flex flex-col items-center">
                     <div className="mx-auto flex sm:w-48 sm:h-48 w-24 h-24 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                       <TiTick className="sm:h-24 sm:w-24 w-12 h-12 text-green-400" aria-hidden="true" />
                     </div>
                     <div className="mt-3 text-center sm:ml-4 sm:mt-3 sm:text-left">
-                      <Dialog.Title as="h3" className="text-base text-center font-semibold leading-6 text-gray-900">
+                      <Dialog.Title as="h3" className="text-lg text-center font-semibold leading-6 text-gray-900">
                         Payment Successful!
                       </Dialog.Title>
                       <div className="mt-2">
-                        <p className="text-sm text-gray-500">
+                        <p className="text-md text-gray-500">
                           Your order number is FGHHG-11221324
                         </p>
                       </div>
                     </div>
                   </div>
-                  
-                  <div className=" mt-4  ">
-                  
-           
-            
-
+                  ):(
+                    <div className="flex flex-col items-center">
+                    <div className="mx-auto flex sm:w-48 sm:h-48 w-24 h-24 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <BiSolidErrorCircle className="sm:h-24 sm:w-24 w-12 h-12 text-red-400" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-3 sm:text-left">
+                      <Dialog.Title as="h3" className="text-lg text-center font-semibold leading-6 text-gray-900">
+                        Payment Failed
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-md text-gray-500">
+                          We couldn't process this payment. Please try another card or payment method. 
+                        </p>
+                      </div>
+                    </div>
                   </div>
+
+                  )}
+                  
 
                   
                 </div>
